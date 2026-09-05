ee7c5dc3a2f76991c87ad671ee7c23843289dd98
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-05 19:30:09 +0500

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,41 @@
+
+public class Employee {
+    private String name;
+    private int employeeid;
+    private double salary;
+    
+    public Employee(String name,int employeeid,double salary)
+    {
+        if(name == null || name.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+            
+        }
+        if(employeeid<=0)
+        {
+            throw new IllegalArgumentException("EmployeeId must be positive");
+        }
+        if(salary<=0)
+        {
+            throw new IllegalArgumentException("salary cannot be negative");
+        }
+        this.name=name.trim();
+        this.employeeid=employeeid;
+        this.salary=salary;
+    }
+    public void setsalary(double salary)
+    {
+        if(salary<0)
+        {
+            throw new IllegalArgumentException("salary cannot be negative");
+        }
+        this.salary=salary;
+    }
+    public void Displayemployeeid()
+    {
+        System.out.println("name : "+name);
+        System.out.println("employee id : "+employeeid);
+        System.out.println("salary : "+salary);
+        
+    }
+}
---
+++ EmployeeTest.java
@@ -0,0 +1,25 @@
+
+public class EmployeeTest {
+    
+    public static void main(String[]args)
+    {
+        try
+        {
+            Employee emp=new Employee("",-135,-25000);
+            emp.Displayemployeeid();
+            try
+            {
+                emp.setsalary(-1000);
+            }
+            catch(IllegalArgumentException e)
+            {
+                System.out.println("Error:"+e.getMessage());
+            }
+        }
+        catch(IllegalArgumentException e)
+        {
+            System.out.println("Error creating Employee:"+e.getMessage());
+        }
+    }
+    
+}
